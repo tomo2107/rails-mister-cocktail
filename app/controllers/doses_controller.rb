7ce625d2e18b44aca5ccd83765d1f11a936ec830
Torsten Motzkus <tmotzkus@web.de>
2020-05-27 22:45:44 +0200

    Cocktail final

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,6 +8,7 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@dose.cocktail)
     else
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,9 +2,5 @@ class Ingredient < ApplicationRecord
   has_many :doses
   has_many :cocktails, through: :doses
 
-  def self.ingredients_array
-    ["lemon", "ice", "mint leaves", "vodka", "tequila", "rhum", "orange juice", "tomato juice"]
-  end
-
   validates :name, uniqueness: true, presence: true
 end
